fff8b8c1715f32d1c295c608632847de7117fa4a
Tomi <szobonyatamas@gmail.com>
2021-04-22 12:24:49 +0200

    upload

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center">Welcome to npxcard 👋</h1>
+<p>
+  <a href="https://www.npmjs.com/package/npxcard" target="_blank">
+    <img alt="Version" src="https://img.shields.io/npm/v/npxcard.svg">
+  </a>
+  <a href="https://github.com/szobonyatomi/npxcard#readme" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://github.com/szobonyatomi/npxcard/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" />
+  </a>
+  <a href="https://github.com/szobonyatomi/npxcard/blob/master/LICENSE" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/github/license/szobonyatomi/npxcard" />
+  </a>
+</p>
+
+> Tomi's npxcard
+
+### 🏠 [Homepage](https://github.com/szobonyatomi/npxcard#readme)
+
+## Run tests
+
+```sh
+npx
+```
+
+## Author
+
+👤 **Tomi Szobonya**
+
+- Github: [@szobonyatomi](https://github.com/szobonyatomi)
+
+## 📝 License
+
+Copyright © 2021 [Tomi Szobonya](https://github.com/szobonyatomi).<br />
+This project is [ISC](https://github.com/szobonyatomi/npxcard/blob/master/LICENSE) licensed.
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+/* szobonyatomi/npxcard
+ *
+ * /index.js - Main Entry point
+ *
+ * coded by tomi@flatLand!
+ * refactored at 22/04/2021
+ */
+
+const chalk = require('chalk');
+const boxen = require('boxen');
+
+// Text + chalk definitions
+
+const data = {
+  name: chalk.white('             Tamas Szobonya'),
+  handle: chalk.white.bold('Tomi'),
+  work: chalk.white('rookie coder at BeCode.org'),
+
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~szobonyatomi'),
+  github: chalk.gray('https://github.com/') + chalk.green('szobonyatomi'),
+  linkedin:
+    chalk.gray('https://linkedin.com/in/') + chalk.blue('tomi-szobonya'),
+  //web: chalk.cyan('#'),
+  npx: chalk.red('npx') + ' ' + chalk.white('Tomi'),
+  labelWork: chalk.white.bold('       Work:'),
+  //labelTwitter: chalk.white.bold('    Twitter:'),
+  labelNpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  //labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:'),
+};
+
+const NEWLINE = '\n';
+const EMPTYLINE = '';
+
+console.log(
+  chalk.green(
+    boxen(
+      [
+        `${data.name} / ${data.handle}`,
+        EMPTYLINE,
+        `${data.labelWork} ${data.work}`,
+        EMPTYLINE,
+        `${data.labelTwitter} ${data.twitter}`,
+        `${data.labelNpm} ${data.npm}`,
+        `${data.labelGitHub} ${data.github}`,
+        `${data.labelLinkedIn} ${data.linkedin}`,
+        `${data.labelWeb} ${data.web}`,
+        EMPTYLINE,
+        `${data.labelCard} ${data.npx}`,
+      ].join(NEWLINE),
+      {
+        padding: 2,
+        margin: 1,
+        borderColor: '#2143ad',
+        borderStyle: 'double',
+        dimBorder: true,
+        float: 'center',
+        align: 'right',
+      }
+    )
+  )
+);
